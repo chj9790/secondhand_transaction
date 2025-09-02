34db465236b14ec830b56f9cae4a91f6a046232e
cwk-cpu <nannuguri3@naver.com>
2025-09-02 11:16:47 +0900

    Create RegionCodeDTO.java

---
+++ 09/choiwonkyung/RegionCodeDTO.java
@@ -0,0 +1,13 @@
+package com.boot.transaction.model;
+
+import lombok.Data;
+
+@Data
+public class RegionCodeDTO {
+	
+	
+	private String code;
+	private String name;
+	private String is_active;
+
+}
